a4a45499a82d5d188bbdd947eebe11cb7b44df46
xiaoyoutaiA <111871861+xiaoyoutaiA@users.noreply.github.com>
2022-09-23 19:29:18 +0800

    Update 2_dense_retrieve.sh

--- OTDAR/2_dense_retrieve.sh
+++ OTDAR/2_dense_retrieve.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python dense_retriever.py \
-	model_file='/root/qty/DAR-main/outputs/2022-09-19/12-36-59/trivia_dar/dpr_biencoder.22' \
+	model_file='path/to/model/nq_dar/dpr_biencoder.24' \
 	qa_dataset=nq_test \
 	ctx_datatsets=[dpr_wiki] \
-	encoded_ctx_files=['/root/qty/DAR-main/generated_embeddings/nq_dar_*'] \
+	encoded_ctx_files=['path/to/generated_embeddings/nq_dar_*'] \
 	out_file='nq_dar_retrieved'
