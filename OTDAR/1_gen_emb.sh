8fa5a37882c2e45cc57a58602a4d7559abf77147
xiaoyoutaiA <111871861+xiaoyoutaiA@users.noreply.github.com>
2022-09-23 19:28:43 +0800

    Update 1_gen_emb.sh

--- OTDAR/1_gen_emb.sh
+++ OTDAR/1_gen_emb.sh
@@ -1,3 +1,3 @@
 # generate document embeddings in parallel
 # e.g. num_shards=4 needs 4 parallel processes
-CUDA_VISIBLE_DEVICES=0 python generate_dense_embeddings.py model_file='/root/qty/DAR-main/outputs/2022-09-19/12-36-59/trivia_dar/dpr_biencoder.22' ctx_src=dpr_wiki shard_id=0 num_shards=4 out_file='nq_dar'
+CUDA_VISIBLE_DEVICES=0 python generate_dense_embeddings.py model_file='path/to/model/nq_dar/dpr_biencoder.24' ctx_src=dpr_wiki shard_id=0 num_shards=4 out_file='nq_dar'
